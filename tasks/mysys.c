f4e22ba71c6ef3177373be4dc853abbfb6f4fd5a
40m41h42t <ikav.css@gmail.com>
2019-03-23 09:47:40 +0800

    add *.c

---
+++ tasks/mysys.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+void mysys(char *command)
+{
+    //pid = fork();
+    pid_t pid;
+    int status;
+    if (command == NULL)
+    {
+        printf("err command\n");
+        return;
+    }
+    pid = fork();
+    if (pid == 0)
+    {
+        execl("/bin/sh", "sh", "-c", command, NULL);
+        exit(123);
+    }
+    else
+    {
+        wait(&status);
+    }
+}
+
+int main()
+{
+    printf("--------------------------------------------------\n");
+    mysys("echo HELLO WORLD");
+    printf("--------------------------------------------------\n");
+    mysys("ls /");
+    printf("--------------------------------------------------\n");
+    return 0;
+}
