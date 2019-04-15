4a9c1cdb2263a67d38f930001550d3052e905b31
40m41h42t <ikav.css@gmail.com>
2019-04-15 16:30:34 +0800

    basic sh3, failed when more than one pipe

--- nuaa/tasks/sh3.c
+++ nuaa/tasks/sh3.c
@@ -18,21 +18,18 @@ void sh_mainloop();
 
 void sh_pipe(char *command);
 
-void sh_dup(char *command, int pipe_read, int pipe_write, BOOL need_wait);
+void sh_dup(char *command, int pipe_read, int pipe_write);
 
 void find_filename(char **dst, char *src);
 
 int split(char *command, int *argc, char *argv[]);
 
-void mysys(char *command, int fd_read, int fd_write, BOOL need_wait);
+void mysys(char *command, int fd_read, int fd_write);
 
 int main()
 {
-
     while (TRUE)
-    {
         sh_mainloop();
-    }
 }
 
 void sh_mainloop()
@@ -58,10 +55,8 @@ void sh_mainloop()
     }
     p = command;
     while (*p && (*p == ' ' || *p == '\n'))
-    {
         ++p;
-    }
-    sh_pipe(p);
+
     // p points at first of command
     if (strstr(p, "cd") == p)
     {
@@ -74,10 +69,8 @@ void sh_mainloop()
         printf("%s\n", getcwd(NULL, 0));
     else if (strstr(p, "exit") == p)
         exit(0);
-    else if (strlen(command) > 0)
-    {
-        sh_pipe(command);
-    }
+    else if (strlen(p) > 0)
+        sh_pipe(p);
     return;
 }
 
@@ -95,12 +88,14 @@ void sh_pipe(char *command)
         if (command[i] == '|')
         {
             command[i] = '\0';
+            // left, input pipe
             p = command + i - 1;
             while (*p == ' ' || *p == '\n')
             {
                 *p = 0;
                 --p;
             }
+            // right, output pipe
             p = command + i + 1;
             while (*p && (*p == ' ' || *p == '\n'))
             {
@@ -113,8 +108,8 @@ void sh_pipe(char *command)
     }
     if (pipe_c == 1)
     {
-        // TODO: single command
-        sh_dup(cmds[0], FD_UNDEF, FD_UNDEF, TRUE);
+        // no pipe
+        sh_dup(cmds[0], FD_UNDEF, FD_UNDEF);
         return;
     }
 
@@ -128,27 +123,23 @@ void sh_pipe(char *command)
     for (i = 0; i < pipe_c; ++i)
     {
         if (i == 0)
-        {
-            sh_dup(cmds[i], FD_UNDEF, fd_pipe[1], TRUE);
-        }
+            // input to pipe
+            sh_dup(cmds[i], FD_UNDEF, fd_pipe[1]);
         else if (i != pipe_c - 1)
-        {
-            // FIXME: read from pipe and write to pipe
-            sh_dup(cmds[i], fd_pipe[0], fd_pipe[1], TRUE);
-        }
+            sh_dup(cmds[i], fd_pipe[0], fd_pipe[1]);
         else
-        {
-            sh_dup(cmds[i], fd_pipe[0], FD_UNDEF, TRUE);
-        }
+            // output from pipe
+            sh_dup(cmds[i], fd_pipe[0], FD_UNDEF);
     }
 }
 
-void sh_dup(char *command, int pipe_read, int pipe_write, BOOL need_wait)
+void sh_dup(char *command, int pipe_read, int pipe_write)
 {
-    int i, len = strlen(command), t;
+    int fd_read = FD_UNDEF, fd_write = FD_UNDEF;
+    int i, len = strlen(command);
     BOOL f_stdout = FALSE, f_stdin = FALSE;
     char *file_input, *file_output;
-    int fd_read = FD_UNDEF, fd_write = FD_UNDEF;
+
     for (i = 0; i < len; ++i)
     {
         if (command[i] == '>')
@@ -165,31 +156,19 @@ void sh_dup(char *command, int pipe_read, int pipe_write, BOOL need_wait)
         }
     }
     if (f_stdin)
-    {
         fd_read = open(file_input, O_CREAT | O_RDWR, 0666);
-    }
     else if (pipe_read > FD_UNDEF)
-    {
         fd_read = pipe_read;
-    }
     else
-    {
         fd_read = FD_UNDEF;
-    }
 
     if (f_stdout)
-    {
         fd_write = open(file_output, O_CREAT | O_RDWR, 0666);
-    }
     else if (pipe_write > FD_UNDEF)
-    {
         fd_write = pipe_write;
-    }
     else
-    {
         fd_write = FD_UNDEF;
-    }
-    mysys(command, fd_read, fd_write, need_wait);
+    mysys(command, fd_read, fd_write);
 }
 
 void find_filename(char **dst, char *src)
@@ -203,13 +182,12 @@ void find_filename(char **dst, char *src)
             src[i] = '\0';
 }
 
-void mysys(char *command, int fd_read, int fd_write, BOOL need_wait)
+void mysys(char *command, int fd_read, int fd_write)
 {
     pid_t pid;
-    char *p = NULL;
-    int status;
-    char *argv[ARG_NUM];
-    int argc;
+    int status, argc;
+    char *p = NULL, *argv[ARG_NUM];
+
     split(command, &argc, argv);
     argv[argc] = NULL;
 
@@ -225,10 +203,7 @@ void mysys(char *command, int fd_read, int fd_write, BOOL need_wait)
             close(fd_read);
         if (fd_write != FD_UNDEF)
             close(fd_write);
-        if (need_wait)
-        {
-            waitpid(pid, NULL, 0);
-        }
+        waitpid(pid, NULL, 0);
         return;
     }
     else
@@ -239,9 +214,7 @@ void mysys(char *command, int fd_read, int fd_write, BOOL need_wait)
             status = dup2(fd_read, STDIN_FILENO);
             close(fd_read);
             if (status < 0)
-            {
                 printf("%s dup stdin failed: %s\n", argv[0], strerror(errno));
-            }
         }
         if (fd_write != FD_UNDEF)
         {
@@ -249,26 +222,21 @@ void mysys(char *command, int fd_read, int fd_write, BOOL need_wait)
             status = dup2(fd_write, STDOUT_FILENO);
             close(fd_write);
             if (status < 0)
-            {
                 printf("%s dup stdout failed: %s\n", argv[0], strerror(errno));
-            }
         }
         if (execvp(argv[0], argv) == -1)
-        {
             printf("%s exec failed: %s\n", argv[0], strerror(errno));
-        }
         exit(0);
     }
 }
 
 int split(char *command, int *argc, char *argv[])
 {
-    char buf[ARG_MAX + 1];
+    char buf[ARG_MAX + 1], split_str[] = " ";
+    char *str = buf, *p = NULL;
+
     memset(buf, 0, sizeof(char) * (ARG_MAX + 1));
     strncpy(buf, command, strlen(command));
-    char *str = buf;
-    char split_str[] = " ";
-    char *p = NULL;
     *argc = 0;
     argv[*argc] = strtok(str, split_str);
     ++*argc;
