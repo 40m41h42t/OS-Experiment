a0649647f25510ef271e76039ed24a43e7df6705
40m41h42t <ikav.css@gmail.com>
2019-04-15 16:14:32 +0800

    a simple sh3

--- nuaa/tasks/sh3.c
+++ nuaa/tasks/sh3.c
@@ -4,27 +4,27 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <errno.h>
 
 #define BOOL int
 #define TRUE 1
 #define FALSE 0
 #define ARG_MAX 256
 #define FILE_NAME_MAX 128
-#define PIPE_NUM 64
+#define ARG_NUM 64
+#define FD_UNDEF -1
 
 void sh_mainloop();
 
 void sh_pipe(char *command);
 
+void sh_dup(char *command, int pipe_read, int pipe_write, BOOL need_wait);
+
+void find_filename(char **dst, char *src);
+
 int split(char *command, int *argc, char *argv[]);
 
-int mysys(char *command);
-
-int my_cd(char *args);
-
-int my_pwd(char *args);
-
-int my_exit(char *args);
+void mysys(char *command, int fd_read, int fd_write, BOOL need_wait);
 
 int main()
 {
@@ -66,8 +66,8 @@ void sh_mainloop()
     if (strstr(p, "cd") == p)
     {
         sscanf(p, "cd %s", argv);
-        int errno = chdir(argv);
-        if (errno != 0)
+        int status = chdir(argv);
+        if (status != 0)
             printf("cd: no such file or directory: %s\n", argv);
     }
     else if (strstr(p, "pwd") == p)
@@ -76,8 +76,7 @@ void sh_mainloop()
         exit(0);
     else if (strlen(command) > 0)
     {
-        mysys(command);
-        return;
+        sh_pipe(command);
     }
     return;
 }
@@ -88,9 +87,9 @@ void sh_pipe(char *command)
     int len = strlen(command);
     int i = 0, status;
     char *p;
-    char *cmds[PIPE_NUM];
+    char *cmds[ARG_NUM];
     int pipe_c = 0;
-    cmds[pipe_c] = command;
+    cmds[pipe_c++] = command;
     for (i = 0; i < len; ++i)
     {
         if (command[i] == '|')
@@ -112,27 +111,153 @@ void sh_pipe(char *command)
             cmds[pipe_c++] = p;
         }
     }
-    if(pipe_c == 1){
+    if (pipe_c == 1)
+    {
         // TODO: single command
+        sh_dup(cmds[0], FD_UNDEF, FD_UNDEF, TRUE);
         return;
     }
-    
+
     status = pipe(fd_pipe);
-    if(status == -1){
+    if (status == -1)
+    {
         printf("can't open pipe\n");
         return;
     }
 
-    for(i = 0;i < pipe_c; ++i){
-        if(i == 0){
-            // TODO: write_to_pipe
+    for (i = 0; i < pipe_c; ++i)
+    {
+        if (i == 0)
+        {
+            sh_dup(cmds[i], FD_UNDEF, fd_pipe[1], TRUE);
         }
-        else if(i != pipe_c -1){
-            // TODO: read from pipe and write to pipe
+        else if (i != pipe_c - 1)
+        {
+            // FIXME: read from pipe and write to pipe
+            sh_dup(cmds[i], fd_pipe[0], fd_pipe[1], TRUE);
         }
-        else{
-            // TODO: write to pipe
+        else
+        {
+            sh_dup(cmds[i], fd_pipe[0], FD_UNDEF, TRUE);
         }
+    }
+}
+
+void sh_dup(char *command, int pipe_read, int pipe_write, BOOL need_wait)
+{
+    int i, len = strlen(command), t;
+    BOOL f_stdout = FALSE, f_stdin = FALSE;
+    char *file_input, *file_output;
+    int fd_read = FD_UNDEF, fd_write = FD_UNDEF;
+    for (i = 0; i < len; ++i)
+    {
+        if (command[i] == '>')
+        {
+            command[i] = '\0';
+            find_filename(&file_output, command + i + 1);
+            f_stdout = TRUE;
+        }
+        else if (command[i] == '<')
+        {
+            command[i] = '\0';
+            find_filename(&file_input, command + i + 1);
+            f_stdin = TRUE;
+        }
+    }
+    if (f_stdin)
+    {
+        fd_read = open(file_input, O_CREAT | O_RDWR, 0666);
+    }
+    else if (pipe_read > FD_UNDEF)
+    {
+        fd_read = pipe_read;
+    }
+    else
+    {
+        fd_read = FD_UNDEF;
+    }
+
+    if (f_stdout)
+    {
+        fd_write = open(file_output, O_CREAT | O_RDWR, 0666);
+    }
+    else if (pipe_write > FD_UNDEF)
+    {
+        fd_write = pipe_write;
+    }
+    else
+    {
+        fd_write = FD_UNDEF;
+    }
+    mysys(command, fd_read, fd_write, need_wait);
+}
+
+void find_filename(char **dst, char *src)
+{
+    int i;
+    while (*src == ' ')
+        ++src;
+    *dst = src;
+    for (i = 0; i < strlen(src); ++i)
+        if (src[i] == ' ' || src[i] == '\n')
+            src[i] = '\0';
+}
+
+void mysys(char *command, int fd_read, int fd_write, BOOL need_wait)
+{
+    pid_t pid;
+    char *p = NULL;
+    int status;
+    char *argv[ARG_NUM];
+    int argc;
+    split(command, &argc, argv);
+    argv[argc] = NULL;
+
+    pid = fork();
+    if (pid == -1)
+    {
+        printf("fork failed\n");
+        exit(-1);
+    }
+    else if (pid != 0)
+    {
+        if (fd_read != FD_UNDEF)
+            close(fd_read);
+        if (fd_write != FD_UNDEF)
+            close(fd_write);
+        if (need_wait)
+        {
+            waitpid(pid, NULL, 0);
+        }
+        return;
+    }
+    else
+    {
+        if (fd_read != FD_UNDEF)
+        {
+            close(STDIN_FILENO);
+            status = dup2(fd_read, STDIN_FILENO);
+            close(fd_read);
+            if (status < 0)
+            {
+                printf("%s dup stdin failed: %s\n", argv[0], strerror(errno));
+            }
+        }
+        if (fd_write != FD_UNDEF)
+        {
+            close(STDOUT_FILENO);
+            status = dup2(fd_write, STDOUT_FILENO);
+            close(fd_write);
+            if (status < 0)
+            {
+                printf("%s dup stdout failed: %s\n", argv[0], strerror(errno));
+            }
+        }
+        if (execvp(argv[0], argv) == -1)
+        {
+            printf("%s exec failed: %s\n", argv[0], strerror(errno));
+        }
+        exit(0);
     }
 }
 
@@ -152,73 +277,5 @@ int split(char *command, int *argc, char *argv[])
         argv[*argc] = p;
         ++*argc;
     }
-    return 0;
-}
-
-int mysys(char *command)
-{
-    // init variables
-
-    // pid variables
-    pid_t pid;
-    int status;
-    // command variables
-    char *argv[100];
-    int argc;
-    // redirect variables
-    int redir_loc;
-    char redir_filename[FILE_NAME_MAX + 1];
-    int fd;
-    // temp variables
-    int i;
-    int j;
-
-    if (command == NULL)
-        return 0;
-
-    // at first, check if need redirection
-    BOOL redir_flag = FALSE;
-    for (i = 0; i < strlen(command); i++)
-        if (command[i] == '>')
-        {
-            redir_flag = TRUE;
-            break;
-        }
-    if (redir_flag)
-    {
-        redir_loc = i;
-        // memcpy from '>'+1 to end of command.
-        // However, space is unuseful which need delete.
-        for (i = i + 1, j = 0; i < strlen(command) && j < FILE_NAME_MAX; ++i)
-            if (command[i] != ' ')
-            {
-                redir_filename[j] = command[i];
-                ++j;
-            }
-        redir_filename[j] = '\0';
-        command[redir_loc] = '\0';
-    }
-    // split input command
-    split(command, &argc, argv);
-    argv[argc] = NULL;
-
-    // while command isn't NULL,
-    // fork a new process to execute
-    pid = fork();
-    // stdin;
-    if (pid == 0)
-    {
-        if (redir_flag)
-        {
-            fd = open(redir_filename, O_CREAT | O_RDWR, 0666);
-            dup2(fd, STDOUT_FILENO);
-            close(fd);
-        }
-        if (execvp(argv[0], argv) < 0)
-            perror("execvp");
-        exit(-1);
-    }
-    else
-        wait(&status);
     return 0;
 }
